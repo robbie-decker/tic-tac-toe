a99617fecc9ff064c0d1f77cebe790f652e535c4
Robbie Decker <robbd56@gmail.com>
2023-08-09 22:04:13 -0400

    Quick first passthrough

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Is this working all right?");
+
+// Need an object for the board
+const gameBoard = (() => {
+    const board = [[], [], []];
+})();
+const displayController = (() => {
+
+})();
