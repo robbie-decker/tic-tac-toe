a42565923956239400e431db4d75470fe6ed4e47
Robbie Decker <robbd56@gmail.com>
2023-08-18 17:03:42 -0400

    Allow page to render gameBoard with given array

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-console.log("Is this working all right?");
 
 // Need an object for the board
 const gameBoard = (() => {
-    const board = [[], [], []];
+    let board = ['x', 'x', 'x', 'o', '', '', '', '', 'x'];
+    const clearBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+    } 
+    // Getter function to return gameboard
+    const getBoard = () => board
+    return{getBoard, clearBoard}
 })();
-const displayController = (() => {
 
-})();
+const displayController = ((game) => {
+    cells = document.getElementsByClassName('cell');
+    // Go through gameboard and render shape in the appropriate spot
+    const renderBoard = () => {
+        for([index, value] of game.getBoard().entries()){
+            cells[index].innerText = value;
+        }
+    }
+    return{renderBoard}
+})(gameBoard);
+
+const players = () =>{
+
+}
+
+
+displayController.renderBoard();
